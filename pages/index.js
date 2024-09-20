c4b2d65b7456c6062a87173237d738815532416f
gravesja <gravesja@kean.edu>
2024-09-20 15:41:34 -0400

    Updated links and fixed navigation

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,27 @@
-import Head from "next/head";
-import Image from "next/image";
-import localFont from "next/font/local";
-import styles from "@/styles/Home.module.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to My Portfolio</h1>
-      <p>Hi, I’m [Your Name], a Junior Network Administrator.</p>
-      <a href="/about">About Me</a>
-      <a href="/portfolio">Portfolio</a>
-      <a href="/contact">Contact</a>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/portfolio">
+              <a>Portfolio</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">
+              <a>Contact</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
