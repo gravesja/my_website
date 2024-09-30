06dcc378ccb6f95feed122d58d7ed5adc18c7ee9
gravesja <gravesja@kean.edu>
2024-09-30 10:36:09 -0400

    Update description for the specific changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+export const content = ["./src/**/*.{html,js}"];
+export const theme = { extend: {}, };
+export const plugins = [];
 
