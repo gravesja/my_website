522a97c560200f4e0688075fa9cf5945212d64ad
gravesja <gravesja@kean.edu>
2024-10-07 13:15:21 -0400

    updated index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-white flex flex-col justify-center items-center">
+    <div className="min-h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-white flex flex-col justify-center items-center bg-animate-wave">
       <h1 className="text-5xl font-extrabold mb-6">Welcome to My Portfolio</h1>
       <p className="mb-8 text-lg max-w-lg text-center">
         Explore my journey as an IT professional. From networking to desktop support to web development, I blend technology with creativity.
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,3 +3,23 @@ export const content = ["./src/**/*.{html,js}"];
 export const theme = { extend: {}, };
 export const plugins = [];
 
+module.exports = {
+    theme: {
+      extend: {
+        animation: {
+          'wave': 'gradientWave 15s ease infinite',
+        },
+        keyframes: {
+          gradientWave: {
+            '0%, 100%': { 'background-position': '0% 50%' },
+            '50%': { 'background-position': '100% 50%' },
+          },
+        },
+        backgroundSize: {
+          'wave': '400% 400%',
+        }
+      },
+    },
+    plugins: [],
+  }
+  
