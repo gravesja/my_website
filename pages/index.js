1b180a3f4537349ac5b6d2fa61ef2a991f13ed5e
gravesja <gravesja@kean.edu>
2025-03-17 11:18:02 -0400

    adjust index page

--- pages/index.js
+++ pages/index.js
@@ -24,23 +24,22 @@ export default function Home() {
         Explore my journey as an IT professional. From networking to desktop support to web development, I blend technology with creativity.
       </p>
       <nav>
-        <ul className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
-          <li>
-            <Link href="/about">
-              <a className="px-4 py-2 bg-white text-purple-700 font-semibold rounded-lg shadow-md hover:bg-purple-100">About Me</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/portfolio">
-              <a className="px-4 py-2 bg-white text-purple-700 font-semibold rounded-lg shadow-md hover:bg-purple-100">Portfolio</a>
-            </Link>
-          </li>
-          
-          <li>
-            <Link href="https://github.com/gravesja">
-              <a className="px-4 py-2 bg-white text-purple-700 font-semibold rounded-lg shadow-md hover:bg-purple-100">Github</a>
-            </Link>
-          </li>
+  <ul className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-8"> 
+    <li>
+      <Link href="/about">
+        <a className="px-4 py-2 bg-white text-purple-700 font-semibold rounded-lg shadow-md hover:bg-purple-100">About Me</a>
+      </Link>
+    </li>
+    <li>
+      <Link href="/portfolio">
+        <a className="px-4 py-2 bg-white text-purple-700 font-semibold rounded-lg shadow-md hover:bg-purple-100">Portfolio</a>
+      </Link>
+    </li>
+    <li>
+      <Link href="https://github.com/gravesja">
+        <a className="px-4 py-2 bg-white text-purple-700 font-semibold rounded-lg shadow-md hover:bg-purple-100">Github</a>
+      </Link>
+    </li>
           
           {/* 
           <li>
